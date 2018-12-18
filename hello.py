998482ac187553d090bc9c87519d2959192de5c5
Kihoon Sohn <38110867+haoapple@users.noreply.github.com>
2018-12-18 11:15:47 -0500

    teaching material for the github lesson

---
+++ hello.py
@@ -0,0 +1,4 @@
+# This is a teaching material for the Data Science Immersive class 
+# Learning objects: how to create branch, modify materials, create and merge pull requests
+
+print("helloworld")
